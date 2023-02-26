f7d99128200043733c17412a087bec452eb6d44d
Eisson Alipio <eisson.alipio@gmail.com>
2023-02-26 00:22:29 -0500

    feat: aspect ratio style for add post

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,5 +1,11 @@
 import { useNavigate } from 'react-router-dom';
 
+import {
+  VERTICAL_IMG,
+  PLACEHOLDER_IMG,
+  HORIZONTAL_IMG,
+} from '../constants/routes';
+
 export interface IAddPostProps {}
 
 export function AddPost(props: IAddPostProps) {
@@ -24,15 +30,15 @@ export function AddPost(props: IAddPostProps) {
         <input
           className="p-2 border w-full font-bold"
           type="text"
-          placeholder="https://i.bb.co/f1VvzJQ/solarpunk-1.jpg"
+          placeholder={PLACEHOLDER_IMG}
           name="imageURL"
         />
       </div>
 
-      <div className="w-full bg-black p-2">
+      <div className="sm:w-1/4 bg-black flex justify-center items-center w-full">
         <img
-          src="https://images.unsplash.com/photo-1487530811176-3780de880c2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
-          alt=""
+          className="bg-contain object-scale-down aspect-[1/1]"
+          src={VERTICAL_IMG}
         />
       </div>
 
@@ -40,7 +46,7 @@ export function AddPost(props: IAddPostProps) {
         Give it a Name
       </div>
 
-      <div className="w-full p-4 bg-green-400 mb-4">
+      <div className="w-full p-4 mb-4">
         <input
           className="p-2 border w-full font-bold"
           type="text"
@@ -48,11 +54,21 @@ export function AddPost(props: IAddPostProps) {
         />
       </div>
 
-      <div className="w-full bg-pink-400">
-        <div className="h-32">image filter component</div>
+      <div className="relative overflow-auto w-full">
+        <div className="flex flex-row gap-2 flex-nowrap">
+          <div className="w-2/5 flex-none h-32 bg-slate-400"> filter 1</div>
+          <div className="w-2/5 flex-none h-32 bg-slate-400"> filter 2</div>
+          <div className="w-2/5 flex-none h-32 bg-slate-400"> filter 3</div>
+          <div className="w-2/5 flex-none h-32 bg-slate-400"> filter 4</div>
+          <div className="w-2/5 flex-none h-32 bg-slate-400"> filter 5</div>
+          <div className="w-2/5 flex-none h-32 bg-slate-400"> filter 6</div>
+          <div className="w-2/5 flex-none h-32 bg-slate-400"> filter 7</div>
+          <div className="w-2/5 flex-none h-32 bg-slate-400"> filter 8</div>
+          <div className="w-2/5 flex-none h-32 bg-slate-400"> filter 9</div>
+        </div>
       </div>
 
-      <div className="w-full mx-auto bg-gray-400 p-4">
+      <div className="w-full mx-auto bg-green-400 p-4">
         <button
           onClick={handlePostNow}
           className="bg-black text-white p-4 w-full rounded"
--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,2 +1,7 @@
 export const TIME_LINE = 'timeline';
 export const ADD_POST = 'add-post';
+
+export const PLACEHOLDER_IMG = 'https://i.bb.co/f1VvzJQ/solarpunk-1.jpg';
+export const VERTICAL_IMG = 'https://wallpaperaccess.com/full/422883.jpg';
+export const HORIZONTAL_IMG =
+  'https://thumbs.dreamstime.com/b/aerial-view-lago-antorno-dolomites-lake-mountain-landscape-alps-peak-misurina-cortina-di-ampezzo-italy-reflected-103752677.jpg';

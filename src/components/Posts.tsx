813612c3c6d1f9a613f41e26d9e847a897f3bfb3
Eisson Alipio <eisson.alipio@gmail.com>
2023-02-27 19:27:25 -0500

    fix: like button fixed and infinite scroll commented since is in conflict with like feature

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,44 +1,48 @@
-import React, { useCallback, useRef } from 'react';
+import React from 'react';
 import * as timeago from 'timeago.js';
 import { HeartIcon } from '@heroicons/react/24/solid';
 
 import ImageCard from './ImageCard';
 import { IPost } from '../store/imageStore';
 import { useImageStore } from '../store/imageStore';
-import { grabTwoByTwo } from '../utils/array';
+// import { grabTwoByTwo } from '../utils/array';
 
 interface IPostsProps {}
 
 export const Posts: React.FunctionComponent<IPostsProps> = () => {
   const likePost = useImageStore((state) => state.likePost);
   const posts = useImageStore((state) => state.posts);
-  const [hasMore, setHasMore] = React.useState<boolean>(posts.length > 0);
 
-  const [counter, setCounter] = React.useState<number>(2);
-  const [postsToRender, setPostToRender] = React.useState(grabTwoByTwo(posts));
+  /**
+   * commented infinite scroll implementation since is raising issues with the like feature
+   */
+  //const [hasMore, setHasMore] = React.useState<boolean>(posts.length > 0);
+
+  //const [counter, setCounter] = React.useState<number>(2);
+  //const [postsToRender, setPostToRender] = React.useState(grabTwoByTwo(posts));
 
   // infinite scroll implementation
-  const observer = useRef<null | IntersectionObserver>();
-  const lastItemRef = useCallback(
-    (node: SVGSVGElement) => {
-      if (observer.current) observer.current.disconnect();
-      observer.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting && hasMore) {
-          if (hasMore) {
-            setCounter((prev) => prev + 2);
-            const toRender = grabTwoByTwo(posts, counter + 2);
-            setPostToRender(toRender);
-            const hasMore__ = posts.length > toRender.length;
-            setHasMore(hasMore__);
-          }
-        }
-      });
-      if (node) observer.current.observe(node);
-    },
-    [hasMore, counter]
-  );
+  // const observer = React.useRef<null | IntersectionObserver>();
+  // const lastItemRef = React.useCallback(
+  //   (node: SVGSVGElement) => {
+  //     if (observer.current) observer.current.disconnect();
+  //     observer.current = new IntersectionObserver((entries) => {
+  //       if (entries[0].isIntersecting && hasMore) {
+  //         if (hasMore) {
+  //           setCounter((prev) => prev + 2);
+  //           const toRender = grabTwoByTwo(posts, counter + 2);
+  //           setPostToRender(toRender);
+  //           const hasMore__ = posts.length > toRender.length;
+  //           setHasMore(hasMore__);
+  //         }
+  //       }
+  //     });
+  //     if (node) observer.current.observe(node);
+  //   },
+  //   [hasMore, counter]
+  // );
 
-  const list = postsToRender.map((post: IPost, index) => {
+  const list = posts.map((post: IPost, index) => {
     return (
       <div className="mt-4 relative" key={post.id}>
         <div className="text-sm font-bold ml-1">{post.title}</div>
@@ -56,7 +60,12 @@ export const Posts: React.FunctionComponent<IPostsProps> = () => {
             likePost(post);
           }}
         >
-          {postsToRender.length !== index + 1 && (
+          <HeartIcon
+            className={`h-8 w-8 ${
+              post.liked ? 'text-red-600' : 'text-gray-100'
+            }`}
+          />
+          {/* {posts.length !== index + 1 && (
             <HeartIcon
               className={`h-8 w-8 ${
                 post.liked ? 'text-red-600' : 'text-gray-100'
@@ -64,14 +73,14 @@ export const Posts: React.FunctionComponent<IPostsProps> = () => {
             />
           )}
 
-          {postsToRender.length === index + 1 && (
+          {posts.length === index + 1 && (
             <HeartIcon
               ref={lastItemRef}
               className={`h-8 w-8 ${
                 post.liked ? 'text-red-600' : 'text-gray-100'
               }`}
             />
-          )}
+          )} */}
         </div>
       </div>
     );
